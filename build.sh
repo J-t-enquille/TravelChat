32dbb9a3e93b39c67ac2d2378fc47fd37640517b
Lekiios <lorenzo.lacz@gmail.com>
2025-06-06 11:40:03 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -28,7 +28,8 @@ npm run build
 cd .. || exit 1
 echo "Copying server build to dist directory..."
 cp -r server/dist/* dist/
-cp -r server/node_modules dist/
+cp -r server/package.json dist/
+cp -r server/package-lock.json dist/
 
 cd client || exit 1
 if [ -d "node_modules" ]; then
@@ -45,5 +46,8 @@ cd .. || exit 1
 echo "Copying client build to dist/client directory..."
 cp -r client/dist/* dist/client
 
+cd dist || exit 1
+echo "Installing production dependencies..."
+npm install --omit=dev
 
 echo "Build completed successfully!"
